a1e4d39553c9ff32d9557ee01a67ea95ed94685f
Bogdan Ailiesei <ailiesei@adobe.com>
2021-06-04 13:40:04 +0300

    Rewrite consul_service_windows to custom resource

---
+++ resources/service_windows.rb
@@ -0,0 +1,57 @@
+#
+# Cookbook: consul
+# License: Apache 2.0
+#
+# Copyright:: 2014-2016, Bloomberg Finance L.P.
+#
+provides :consul_service, os: windows
+default_action :enable
+unified_mode true
+
+action :enable do
+  directories = %W(#{new_resource.data_dir}
+                   #{new_resource.config_dir}
+                   #{::File.dirname(new_resource.nssm_params['AppStdout'])}
+                   #{::File.dirname(new_resource.nssm_params['AppStderr'])}).uniq.compact
+
+  directories.delete_if { |i| i.eql? '.' }.each do |dirname|
+    directory dirname do
+      recursive true
+    end
+  end
+
+  nssm 'consul' do
+    program new_resource.program
+    args command(new_resource.config_file, new_resource.config_dir)
+    parameters new_resource.nssm_params.select { |_k, v| v != '' }
+    action :install
+  end
+end
+
+action :reload do
+  execute 'Reload consul' do
+    command 'consul.exe reload' + (new_resource.acl_token ? " -token=#{new_resource.acl_token}" : '')
+    cwd ::File.dirname(new_resource.program)
+    action :run
+  end
+end
+
+action :restart do
+  powershell_script 'Restart consul' do
+    code 'restart-service consul'
+  end
+end
+
+action :disable do
+  nssm 'consul' do
+    action %i(stop remove)
+  end
+
+  file new_resource.config_file do
+    action :delete
+  end
+end
+
+action_class do
+  include ConsulCookbook::Helpers
+end
